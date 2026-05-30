0c8a7e277ca2f6d2fe43914a66093937122986bc
frank1411 <fsanta01@gmail.com>
2026-05-30 13:19:32 -0400

    Revert db.ts to standard singleton to troubleshoot 500 error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,21 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const createPrismaClient = () => {
-  return new PrismaClient({
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query'] : ['error'],
   })
-}
 
-const dbProxy = new Proxy({} as PrismaClient, {
-  get(target, prop, receiver) {
-    if (!globalForPrisma.prisma) {
-      globalForPrisma.prisma = createPrismaClient();
-    }
-    const client = globalForPrisma.prisma!;
-    const value = Reflect.get(client, prop, receiver);
-    return typeof value === 'function' ? value.bind(client) : value;
-  },
-});
-
-export const db = dbProxy;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
